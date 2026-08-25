2912bca77a4edb56d83ab1f4db86563fe1683ec4
Atino Sandra <atinosandra@Atinos-MacBook-Pro.local>
2026-08-25 15:21:17 +0200

    add .env file

---
+++ video_stats.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv(dotenv_path="./.env")
+
+API_KEY = "AIzaSyBhZE3KMiPECY3kQhLg5Jg9Bx6Gdbv4ORQ"
+
+CHANNEL_HANDLE = "BusinessInsider"
+
+
+def getplaylist_id():
+    try:
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=ContentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        # print(response)
+
+        data = response.json()
+        
+        #print(json.dumps(data, indent=4))
+        channel_items = data['items'][0]
+        channel_playlistId = channel_items['contentDetails']['relatedPlaylists']['uploads']
+        print(channel_playlistId)
+        return channel_playlistId
+
+    except requests.exceptions.HTTPError as e:
+        raise e
+
+if __name__ == "__main__":
+  getplaylist_id()
