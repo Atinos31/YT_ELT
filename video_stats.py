ddf6044f066b17538406b1a47eb5501f8c2d75c0
SandraDev <atinosandra@Atinos-MacBook-Pro.local>
2026-08-26 11:03:15 +0200

    unique video ids

--- video_stats.py
+++ video_stats.py
@@ -1,3 +1,4 @@
+
 import requests
 import json
 import os
@@ -7,6 +8,7 @@ load_dotenv(dotenv_path="./.env")
 API_KEY = os.getenv("YOUTUBE_API_KEY")
 
 CHANNEL_HANDLE = "BusinessInsider"
+maxResults = 50
 
 
 def getplaylist_id():
@@ -29,5 +31,31 @@ def getplaylist_id():
     except requests.exceptions.HTTPError as e:
         raise e
 
-if __name__ == "__main__":
-  getplaylist_id()
+def get_video_ids(playlistId):
+    video_ids = []
+    pageToken = None
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=ContentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            for item in data.get("items",[]):
+                video_id = item["contentDetails"]['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get("nextPageToken")
+            if not pageToken:
+                break
+        return video_ids
+
+    except requests.exceptions.RequestExeption as e:
+        raise e
+
+
+if  __name__ == "__main__":
+    playlistId = getplaylist_id()
+    get_video_ids(playlistId)
